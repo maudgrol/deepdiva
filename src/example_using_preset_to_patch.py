29987089c19f3a23b57b3dfc0179dffdbda1f225
Graham Herdman <herdmanctg@outlook.com>
2021-09-14 19:56:51 +0200

    update example file

--- src/example_using_preset_to_patch.py
+++ src/example_using_preset_to_patch.py
@@ -1,9 +1,14 @@
 from utils.transformer import preset_to_patch
+from utils.transformer import patch_to_preset
 
 
-x= preset_to_patch("../data/h2p_example.h2p")
-print(x)
+x = preset_to_patch("/Users/grahamherdman/Documents/data-science-retreat/deep-diva/deepdiva/data/MS-Rev1.h2p")
+# print(x)
 
-#if you like to NOT normalize the preset to patch
-x= preset_to_patch("../data/h2p_example.h2p", normal=False)
-print(x)
+# #if you like to NOT normalize the preset to patch
+# x_norm = preset_to_patch("../data/h2p_example.h2p", normal=False)
+# print(x_norm)
+
+
+x_preset = patch_to_preset(x, "/Users/grahamherdman/Documents/data-science-retreat/deep-diva/deepdiva/data/patch_to_preset_test.h2p")
+print(x_preset)
