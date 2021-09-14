a1cd74d9091da357d6c41b42befed6004c6552eb
Selbo <sebastian.eppner@gmail.com>
2021-09-14 15:09:22 +0200

    copy the norm_function to utils

---
+++ src/utils/norm_functions.py
@@ -0,0 +1,113 @@
+def norm_placeholder(x):
+    return 0
+
+def norm_float_0_1(x):
+    return x
+
+def norm_float_0_4(x):
+    return x/4
+
+def norm_float_0_100(x):
+    return x/100
+
+def norm_float_0_150(x):
+    return x/150
+
+def norm_float_0_200(x):
+    return x/200
+
+def norm_float_0_250(x):
+    return x/250
+
+def norm_float_1_9(x):
+    return (x-1)/8
+
+def norm_float_1_16(x):
+    return (x-1)/15
+
+def norm_float_1_200(x):
+    return (x-1)/199
+
+def norm_float_2_100(x):
+    return (x-2)/98
+
+def norm_float_5_5(x):
+    return (x+5) / 10
+
+def norm_float_8_8(x):
+    return (x+8) / 16
+
+def norm_float_20_20(x):
+    return (x+20) / 40
+
+def norm_float_24_24(x):
+    return (x+24) / 48
+
+def norm_float_30_30(x):
+    return (x+30) / 60
+
+def norm_float_30_150(x):
+    return (x-30) / 120
+
+def norm_float_50_50(x):
+    return (x+50) / 100
+
+def norm_float_50_200(x):
+    return (x-50) / 150
+
+def norm_float_100_100(x):
+    return (x+100) / 200
+
+def norm_float_120_120(x):
+    return (x+120) / 240
+
+def norm_int_0_1(x):
+    return x
+
+def norm_int_0_2(x):
+    return x/2
+
+def norm_int_0_3(x):
+    return x/3
+
+def norm_int_0_4(x):
+    return x/4
+
+def norm_int_0_5(x):
+    return x/5
+
+def norm_int_0_6(x):
+    return x/6
+
+def norm_int_0_7(x):
+    return x/7
+
+def norm_int_0_12(x):
+    return x/12
+
+def norm_int_0_23(x):
+    return x/23
+
+def norm_int_0_26(x):
+    return x/26
+
+def norm_int_1_2(x):
+    return x-1
+
+def norm_int_1_4(x):
+    return (x-1) /3
+
+def norm_int_3_23(x):
+    return (x-3) /20
+
+def norm_int_24_24(x):
+    return (x+24) /48
+
+def norm_fixed_octave(x):
+    return (x+24) / 48
+
+def norm_int_m1_3(x):
+    return (x+1) / 4
+
+def norm_int_m3_23(x):
+    return (x+3) / 26
