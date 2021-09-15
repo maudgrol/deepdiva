3dad64c2d18662316d4951e2d4aa339855ad5b11
Selbo <sebastian.eppner@gmail.com>
2021-09-15 17:35:58 +0200

    just in case you want to see the latest code i used for data generation

--- src/Sebastian_Restricted_Parameters_Datageneration.py
+++ src/Sebastian_Restricted_Parameters_Datageneration.py
@@ -27,12 +27,11 @@ base_patch = preset_to_patch("../data/MS-REV1_deepdiva.h2p")
 
 #i only want to vary 4 parameters, so all other 278 should be overridden
 ## i removed 86 tuning oscillator 1, 244 wave oscillator 1, 140 & 148 cutoff, 141& 149 resonance"
-train_parameters, override_parameters = split_train_override_patch(base_patch, [86, 87, 90, 97, 98, 131, 132, 140, 141, 148, 149])
-
+train_parameters, override_parameters = split_train_override_patch(base_patch, [86, 131, 141, 148])
 
 synth = spgl.synth.SynthVST("/Library/Audio/Plug-Ins/VST/u-he/Diva.vst",
                             note_length_secs=1.0,
-                            render_length_secs=3.0,
+                            render_length_secs=1.0,
                             overridden_params= override_parameters)
 
 # Mel-frequency Cepstral Coefficients audio feature extractor.
@@ -41,11 +40,11 @@ features = spgl.features.MFCC(num_mfccs=13, frame_size=2048,
 
 # Setup generator for MFCC output
 
-for i in range(10):
+for i in range(1):
     generator = spgl.DatasetGenerator(synth, features,
-                                      output_folder="../ms20_data_FM_mfcc", save_audio=False)
-    generator.generate(5000, file_prefix=f"train_{i}_")
-    generator.generate(1000, file_prefix=f"test_{i}_")
+                                      output_folder="../small_data", save_audio=True)
+    generator.generate(1000, file_prefix=f"train_{i}_")
+    generator.generate(100, file_prefix=f"test_{i}_")
     generator.save_scaler('data_scaler.pkl')
 
 
