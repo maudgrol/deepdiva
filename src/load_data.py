358c8d10fe7e543d4c2654b60462aa28fc696249
Maud Grol <maudgrol@gmail.com>
2021-09-18 17:07:54 +0200

    adjust filepaths

--- src/load_data.py
+++ src/load_data.py
@@ -1,17 +1,19 @@
 import os
-import spiegelib as spgl
 import numpy as np
 
 DATA_PATH = "../data/"
 
 # Load data files
-trainFeatures = np.load(os.path.join(DATA_PATH, "data_mfcc/train_features.npy"))
-trainParams = np.load(os.path.join(DATA_PATH, "data_mfcc/train_patches.npy"))
-testFeatures = np.load(os.path.join(DATA_PATH, "data_mfcc/test_features.npy"))
-testParams = np.load(os.path.join(DATA_PATH, "data_mfcc/test_patches.npy"))
+trainFeatures = np.load(os.path.join(DATA_PATH, "toy_data/train_0_features.npy"))
+trainParams = np.load(os.path.join(DATA_PATH, "toy_data/train_0_patches.npy"))
+testFeatures = np.load(os.path.join(DATA_PATH, "toy_data/test_0_features.npy"))
+testParams = np.load(os.path.join(DATA_PATH, "toy_data/test_0_patches.npy"))
+
+trainMels = np.load(os.path.join(DATA_PATH, "toy_data/train_0_melspectrogram.npy"))
 
 # Check dimensions of training and test data
 print(f"The shape of trainFeatures: {trainFeatures.shape}")
 print(f"The shape of trainParams: {trainParams.shape}")
 print(f"The shape of testFeatures: {testFeatures.shape}")
 print(f"The shape of testParams: {testParams.shape}")
+print(f"The shape of trainMels: {trainMels.shape}")
