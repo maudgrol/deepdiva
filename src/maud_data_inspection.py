558a5e3705dec7191dee81abb5969ec17899b0e6
Maud Grol <maudgrol@gmail.com>
2021-09-22 18:27:46 +0200

    load relevant data and do not flip melspectrogram again

--- src/maud_data_inspection.py
+++ src/maud_data_inspection.py
@@ -3,31 +3,30 @@ import os
 import numpy as np
 import matplotlib.pyplot as plt
 
-DATA_PATH = "../data/dataset"
+DATA_PATH = "../data/dataset_4params"
 
 # Load data files
-trainAudio = np.load(os.path.join(DATA_PATH, "train_audio_decoded.npy"))
+#trainAudio = np.load(os.path.join(DATA_PATH, "train_audio_decoded.npy"))
 trainMels = np.load(os.path.join(DATA_PATH, "train_melspectrogram.npy"))
-trainMFCC = np.load(os.path.join(DATA_PATH, "train_features.npy"))
+#trainMFCC = np.load(os.path.join(DATA_PATH, "train_features.npy"))
 
-testAudio = np.load(os.path.join(DATA_PATH, "test_audio_decoded.npy"))
+#testAudio = np.load(os.path.join(DATA_PATH, "test_audio_decoded.npy"))
 testMels = np.load(os.path.join(DATA_PATH, "test_melspectrogram.npy"))
-testMFCC = np.load(os.path.join(DATA_PATH, "test_features.npy"))
+#testMFCC = np.load(os.path.join(DATA_PATH, "test_features.npy"))
 
 # Check dimensions of training and test data
-print(f"The shape of trainAudio: {trainAudio.shape}")
+#print(f"The shape of trainAudio: {trainAudio.shape}")
 print(f"The shape of trainMels: {trainMels.shape}")
-print(f"The shape of trainMFCC: {trainMFCC.shape}")
+#print(f"The shape of trainMFCC: {trainMFCC.shape}")
 
-print(f"The shape of testAudio: {testAudio.shape}")
+#print(f"The shape of testAudio: {testAudio.shape}")
 print(f"The shape of testMels: {testMels.shape}")
-print(f"The shape of testMFCC: {testMFCC.shape}")
+#print(f"The shape of testMFCC: {testMFCC.shape}")
 
 
 # Plot decoded audio data
-def plot_audio(filepath, array_file:str, index:int):
-    audio = np.load(os.path.join(filepath, array_file))
-    audio = audio[index].astype("float32")
+def plot_audio(array_file, index:int):
+    audio = array_file[index].astype("float32")
 
     plt.plot(audio) # x = time; y = amplitude (loudness)
     plt.show()
@@ -35,11 +34,11 @@ def plot_audio(filepath, array_file:str, index:int):
 
 
 # Plot mel spectrogram
-def plot_mel(filepath, array_file: str, index: int):
-    mel = np.load(os.path.join(filepath, array_file))
-    mel = mel[index].astype("float32")
+def plot_mel(array_file, index: int):
+    mel = array_file[index]
+    mel = mel[:,:,0].astype("float32")
 
-    plt.imshow(mel[::-1, :], cmap="inferno")  # we flip the image upside down so lower tones are lower in image
+    plt.imshow(mel, cmap="inferno")
     plt.axis("off")
     plt.show()
     plt.close()
