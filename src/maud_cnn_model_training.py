25c2fd39a13c1669706ab3b762f20dbea907e4d7
Maud Grol <maudgrol@gmail.com>
2021-09-23 15:48:29 +0200

    change model initialization and loss function

--- src/maud_cnn_model_training.py
+++ src/maud_cnn_model_training.py
@@ -7,7 +7,7 @@ import matplotlib.pyplot as plt
 from maud_conv_model import ConvModel
 
 
-DATA_PATH = "../data/toy_data"
+DATA_PATH = "../data/dataset_4params"
 MODEL_PATH = "../models"
 BATCH_SIZE = 128
 
@@ -30,42 +30,37 @@ print(f"The shape of testMels: {testMels.shape}")
 print(f"The shape of testTargets: {test_target.shape}")
 
 # Create tensorflow datasets
-train_mel_original = tf.data.Dataset.from_tensor_slices((trainMels, train_target))
-test_mel_original = tf.data.Dataset.from_tensor_slices((testMels, test_target))
-
-# Save datasets - but seems tricky to reload because you need this spec variable saved
-filename = os.path.join(DATA_PATH, "train_dataset_original")
-tf.data.experimental.save(train_mel_original, filename, compression=None, shard_func=None)
-
-filename = os.path.join(DATA_PATH, "test_dataset_original")
-tf.data.experimental.save(test_mel_original, filename, compression=None, shard_func=None)
-
-spec = train_mel_original.element_spec
-
+dataset_train_original = tf.data.Dataset.from_tensor_slices((trainMels, train_target))
+dataset_validate_original = tf.data.Dataset.from_tensor_slices((testMels, test_target))
 
 # Prepare datasets for model training
-mel_dataset_train = train_mel_original.cache().shuffle(10000).batch(BATCH_SIZE)
-mel_dataset_validate = test_mel_original.cache().batch(BATCH_SIZE) # No need to shuffle validation data
-mfcc_dataset_train = train_mfcc_original.cache().shuffle(10000).batch(BATCH_SIZE)
-mfcc_dataset_validate = test_mfcc_original.cache().batch(BATCH_SIZE) # No need to shuffle validation data
+dataset_train = dataset_train_original.cache().shuffle(10000).batch(BATCH_SIZE)
+dataset_validate = dataset_validate_original.cache().batch(BATCH_SIZE) # No need to shuffle validation data
+
+# define custom loss function root mean squared error
+def root_mean_squared_error(y_true, y_pred):
+    return tf.sqrt(tf.mean(tf.square(y_pred - y_true)))
+
 
 # Train model
-model = ConvModel(input_size=64, output_size=4)
+model = ConvModel(shape=(256, 346, 1),
+                  output_size=4)
+
 model.summary()
 
 model.compile(
     optimizer="adam",
-    loss="cosine_similarity"
+    loss="mean_squared_error"
 )
 
 history = model.fit(
-    mel_dataset_train,
-    epochs=50,
-    validation_data=mel_dataset_validate
+    dataset_train,
+    epochs=2,
+    validation_data=dataset_validate
 )
 
-# Save the entire model as a SavedModel.
-model.save(os.path.join(MODEL_PATH, 'maud_conv_model_18sep_1625'))
+# # # Save the entire model as a SavedModel.
+# # model.save(os.path.join(MODEL_PATH, 'maud_conv_model_18sep_1625'))
 
 
 plt.plot(history.history["loss"], label="training loss")
