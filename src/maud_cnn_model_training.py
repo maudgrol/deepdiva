6f10b129d84b22277e973c33efd8c263252d6ea4
Maud Grol <maudgrol@gmail.com>
2021-09-26 14:10:09 +0200

    add custom data generator option

--- src/maud_cnn_model_training.py
+++ src/maud_cnn_model_training.py
@@ -5,8 +5,11 @@ import numpy as np
 import tensorflow as tf
 import matplotlib.pyplot as plt
 from maud_conv_model import ConvModel
+from maud_data_loader import DataGenerator
 
 DATA_PATH = "../data/dataset_124params"
+TRAIN_PATH = "../data/dataset_124params/train_dataset"
+VAL_PATH = "../data/dataset_124params/val_dataset"
 MODEL_PATH = "../models"
 BATCH_SIZE = 64
 
@@ -14,6 +17,7 @@ BATCH_SIZE = 64
 if not os.path.exists(MODEL_PATH):
     os.makedirs(MODEL_PATH)
 
+# OPTION 1: LOAD ALL DATA AT ONCE (LARGE DATASET) --------------------
 # Load data files
 trainMels = np.load(os.path.join(DATA_PATH, "train_melspectrogram.npy"))
 testMels = np.load(os.path.join(DATA_PATH, "test_melspectrogram.npy"))
@@ -32,23 +36,34 @@ print(f"The shape of testTargets: {test_target.shape}")
 dataset_train_original = tf.data.Dataset.from_tensor_slices((trainMels, train_target))
 dataset_validate_original = tf.data.Dataset.from_tensor_slices((testMels, test_target))
 
-# tf.data.experimental.save(dataset_train_original, os.path.join(DATA_PATH, "dataset_train_original"),
-#                           compression=None, shard_func=None)
-# tf.data.experimental.save(dataset_validate_original, os.path.join(DATA_PATH, "dataset_validate_original"),
-#                           compression=None, shard_func=None)
-
 # Prepare datasets for model training
 dataset_train = dataset_train_original.cache().shuffle(10000).batch(BATCH_SIZE)
 dataset_validate = dataset_validate_original.cache().batch(BATCH_SIZE) # No need to shuffle validation data
 
 
+# OPTION 2: USE DATA GENERATOR --------------------------
+# Create list with ids for training and validation data generator
+train_id = list(np.arange(50000))
+val_id = list(np.arange(10000))
+
+# Create data generator instances
+training_generator = DataGenerator(folder=TRAIN_PATH,
+                                   prefix="train_",
+                                   list_ids=train_id,
+                                   batch_size=BATCH_SIZE)
+validation_generator = DataGenerator(folder=VAL_PATH,
+                                     prefix="test_",
+                                     list_ids=val_id,
+                                     batch_size=BATCH_SIZE)
+
+
 # Define custom loss function root mean squared error (MSE punishes big errors relatively more)
 def root_mean_squared_error(y_true, y_pred):
-    return tf.math.sqrt(tf.math.reduce_mean(tf.math.square(y_pred - y_true)))
+    return tf.sqrt(tf.reduce_mean(tf.square(y_pred - y_true)))
 
 
 # Define custom callback saving model's weights every n epochs
-checkpoint_path = os.path.join(MODEL_PATH, "maud_training_24sep_1700/cp-{epoch:03d}.ckpt")
+checkpoint_path = os.path.join(MODEL_PATH, "maud_training_25/cp-{epoch:03d}.ckpt")
 
 cp_callback = tf.keras.callbacks.ModelCheckpoint(
     filepath=checkpoint_path,
@@ -57,7 +72,7 @@ cp_callback = tf.keras.callbacks.ModelCheckpoint(
     save_best_only=False,
     save_weights_only=True,
     mode="auto",
-    save_freq=2*math.ceil(trainMels.shape[0] / BATCH_SIZE)
+    save_freq=20*math.ceil(len(train_id) / BATCH_SIZE)
 )
 
 
@@ -69,13 +84,13 @@ model.summary()
 
 model.compile(
     optimizer="adam",
-    loss="mean_squared_error"
+    loss=root_mean_squared_error
 )
 
 history = model.fit(
-    dataset_train,
-    epochs=3,
-    validation_data=dataset_validate,
+    training_generator,
+    epochs=100,
+    validation_data=validation_generator,
     callbacks=[cp_callback]
 )
 
@@ -88,5 +103,3 @@ plt.plot(history.history["val_loss"], label="validation loss")
 plt.legend()
 plt.show()
 plt.close()
-
-
