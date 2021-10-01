def4dedea8575b767067875bfbac5b7a4b1e5b2f
Maud Grol <maudgrol@gmail.com>
2021-10-01 11:59:21 +0200

    import layers for consistency with lstm

--- src/deepdiva/model/cnn_model.py
+++ src/deepdiva/model/cnn_model.py
@@ -11,57 +11,57 @@ class ConvModel(tf.keras.Model):
 
         # Define all layers
         # Layer of convolutional block 1
-        self.conv1 = tf.keras.layers.Conv2D(filters=32 ,
-                                            kernel_size=(3, 3),
-                                            strides=(1, 1),
-                                            padding="same",
-                                            activation="relu",
-                                            name="Conv_1")
-        self.max1 = tf.keras.layers.MaxPooling2D(pool_size=(2, 2),
-                                                 name="MaxPool_1")
+        self.conv1 = layers.Conv2D(filters=32,
+                                   kernel_size=(3, 3),
+                                   strides=(1, 1),
+                                   padding="same",
+                                   activation="relu",
+                                   name="Conv_1")
+        self.max1 = layers.MaxPooling2D(pool_size=(2, 2),
+                                        name="MaxPool_1")
 
         # Layer of convolutional block 2
-        self.conv2 = tf.keras.layers.Conv2D(filters=64,
-                                            kernel_size=(3, 3),
-                                            strides=(1, 1),
-                                            padding="same",
-                                            activation="relu",
-                                            name="Conv_2")
-        self.max2 = tf.keras.layers.MaxPooling2D(pool_size=(2, 2),
-                                                 name="MaxPool_2")
+        self.conv2 = layers.Conv2D(filters=64,
+                                   kernel_size=(3, 3),
+                                   strides=(1, 1),
+                                   padding="same",
+                                   activation="relu",
+                                   name="Conv_2")
+        self.max2 = layers.MaxPooling2D(pool_size=(2, 2),
+                                        name="MaxPool_2")
 
         # Layer of convolutional block 3
-        self.conv3 = tf.keras.layers.Conv2D(filters=128,
-                                            kernel_size=(3, 3),
-                                            strides=(1, 1),
-                                            padding="same",
-                                            activation="relu",
-                                            name="Conv_3")
-        self.max3 = tf.keras.layers.MaxPooling2D(pool_size=(2, 2),
-                                                 name="MaxPool_3")
+        self.conv3 = layers.Conv2D(filters=128,
+                                   kernel_size=(3, 3),
+                                   strides=(1, 1),
+                                   padding="same",
+                                   activation="relu",
+                                   name="Conv_3")
+        self.max3 = layers.MaxPooling2D(pool_size=(2, 2),
+                                        name="MaxPool_3")
 
         # Layer of convolutional block 4
-        self.conv4 = tf.keras.layers.Conv2D(filters=128,
-                                            kernel_size=(3, 3),
-                                            strides=(1, 1),
-                                            padding="same",
-                                            activation="relu",
-                                            name="Conv_4")
-        self.max4 = tf.keras.layers.MaxPooling2D(pool_size=(2, 2),
-                                                 name="MaxPool_4")
+        self.conv4 = layers.Conv2D(filters=128,
+                                   kernel_size=(3, 3),
+                                   strides=(1, 1),
+                                   padding="same",
+                                   activation="relu",
+                                   name="Conv_4")
+        self.max4 = layers.MaxPooling2D(pool_size=(2, 2),
+                                        name="MaxPool_4")
 
         # Fully connected layers and dropout
-        self.dropout1 = tf.keras.layers.Dropout(rate=0.2,
-                                               name="Dropout_1")
-        self.flatten = tf.keras.layers.Flatten(name="Flatten_1")
-        self.fc1 = tf.keras.layers.Dense(units=256,
-                                         activation="relu",
-                                         name="Dense_1")
-        self.dropout2 = tf.keras.layers.Dropout(rate=0.4,
-                                               name="Dropout_2")
-        self.fc2 = tf.keras.layers.Dense(units=num_outputs,
-                                         activation="linear",
-                                         name="Output_layer")
+        self.dropout1 = layers.Dropout(rate=0.2,
+                                       name="Dropout_1")
+        self.flatten = layers.Flatten(name="Flatten_1")
+        self.fc1 = layers.Dense(units=256,
+                                activation="relu",
+                                name="Dense_1")
+        self.dropout2 = layers.Dropout(rate=0.4,
+                                       name="Dropout_2")
+        self.fc2 = layers.Dense(units=num_outputs,
+                                activation="linear",
+                                name="Output_layer")
 
         self._build_graph()
 
